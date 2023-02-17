f2eccb8690e8ccb38c454e74d32d709a4e0ea2bb
wegon <103645325+wegon@users.noreply.github.com>
2023-02-18 04:49:03 +0800

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ appDiv.innerHTML = `<h1>JS Starter1</h1>`;
 
 import * as m1 from './es6/module/m1.js';
 console.log(m1);
+
+import * as m2 from './es6/module/m2.js';
+console.log(m2);
+
+import * as m3 from './es6/module/m3.js';
+console.log(m3);
