dbec70f955fcdb38556dcfdf3021d1fb75ae27a9
wegon <103645325+wegon@users.noreply.github.com>
2023-02-18 05:17:23 +0800

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Import stylesheets
 import './style.css';
 
+import * as vue3 from './vue3/vue3.js';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter1</h1>`;
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-import * as m1 from './es6/module/m1.js';
-console.log(m1);
+// import * as m1 from './es6/module/m1.js';
+// console.log(m1);
 
-import * as m2 from './es6/module/m2.js';
-console.log(m2);
+// import * as m2 from './es6/module/m2.js';
+// console.log(m2);
 
-import * as m3 from './es6/module/m3.js';
-console.log(m3);
+// import * as m3 from './es6/module/m3.js';
+// console.log(m3);
